461537e30da813d37ddc17b1b02b7369eb6454ab
mitchdzugan <mitchdzugan@gmail.com>
2016-02-25 15:26:50 -0800

    remove directx video backend to escape dependency hell

--- Source/Core/VideoCommon/VideoBackendBase.cpp
+++ Source/Core/VideoCommon/VideoBackendBase.cpp
@@ -10,7 +10,6 @@
 // TODO: ugly
 #ifdef _WIN32
 #include "VideoBackends/D3D/VideoBackend.h"
-#include "VideoBackends/D3D12/VideoBackend.h"
 #endif
 #include "VideoBackends/OGL/VideoBackend.h"
 #include "VideoBackends/Software/VideoBackend.h"
@@ -36,17 +35,6 @@ void VideoBackendBase::PopulateList()
 {
 	// OGL > D3D11 > D3D12 > SW
 	g_available_video_backends.push_back(std::make_unique<OGL::VideoBackend>());
-#ifdef _WIN32
-	g_available_video_backends.push_back(std::make_unique<DX11::VideoBackend>());
-
-	// More robust way to check for D3D12 support than (unreliable) OS version checks.
-	HMODULE d3d12_module = LoadLibraryA("d3d12.dll");
-	if (d3d12_module != nullptr)
-	{
-		FreeLibrary(d3d12_module);
-		g_available_video_backends.push_back(std::make_unique<DX12::VideoBackend>());
-	}
-#endif
 	g_available_video_backends.push_back(std::make_unique<SW::VideoSoftware>());
 
 	const auto iter = std::find_if(g_available_video_backends.begin(), g_available_video_backends.end(), [](const auto& backend) {
